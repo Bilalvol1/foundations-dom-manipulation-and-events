6843c21e093879ff51f6dc93e10b2585f53e6756
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-26 14:18:47 +0530

    method 3

--- events/test.js
+++ events/test.js
@@ -1,3 +1,8 @@
 // method 2
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert('Hello World!');
+
+
+// method 3
 const btn = document.querySelector("#btn");
-btn.onclick = () => alert('Hello World!');
+btn.addEventListener("click", () => alert("Hello World!"))
